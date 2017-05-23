bee70e0f561e65e3a9489cf6e71b1c8a1b5cc530
musicteachj <jimilittle3@hotmail.com>
2017-05-22 23:45:58 -0400

    add
    sound effects

--- game.js
+++ game.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
   var attackerChosen = false;
   var defenderChosen = false;
 
+  // Game Music
   var audioElement = document.createElement("audio");
       audioElement.setAttribute("src", "StarWarsTheme.m4a");
-
   $(".theme-button").on("click", function() {
         audioElement.play();
       });
@@ -26,23 +26,24 @@ $(document).ready(function() {
       defenderChosen = true;
       $("#attack").append("<button class='aButton'> Attack </button>");
       alert("You are now ready to attack!");
-
-
     }
+
+    var audioElement2 = document.createElement("audio");
+      audioElement2.setAttribute("src", "lightsaber.mp3");
 
   $('.aButton').on('click', function() {
     alert('Opponent has been attacked');
     console.log('This is where the scoring is supposed to be... but it\'s fucking hard...');
+      // The lightsaber audio is played when the attack button is clicked
+      audioElement2.play();
+      
+  }) 
   })
-
-    
-  })
-
 });
 
 
 
-
+// Other ideas
 
 /*
 
